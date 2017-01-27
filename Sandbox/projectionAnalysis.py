7bd79044a51d925b72491b4398760a009d9e0f46
Prageeth Jayathissa <p.jayathissa@gmail.com>
2017-01-27 20:23:33 +0100

    plot results

--- Sandbox/projectionAnalysis.py
+++ Sandbox/projectionAnalysis.py
@@ -9,6 +9,7 @@ Date: 27.01.2017
 
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def calcShadow(P0,sunAz,sunAlt,panelAz,panelAlt,h,d):
@@ -23,9 +24,9 @@ def calcShadow(P0,sunAz,sunAlt,panelAz,panelAlt,h,d):
 	#Calcuate the contraction or expansion of the shadow in the x and y directions due to the actuation
 	#The difference is relative to the flat panel position
 	y=(math.sin(sunAlt-panelAlt/2)/(math.sin(math.pi/2-sunAlt)))*h*math.sin(panelAlt/2) 
-
+	print y
 	x=(math.sin(sunAz-panelAz/2)/(math.sin(math.pi/2-sunAz)))*h*math.sin(panelAz/2)
-
+	print x
 
 	"""
 	     S1
@@ -35,11 +36,17 @@ def calcShadow(P0,sunAz,sunAlt,panelAz,panelAlt,h,d):
 	    \/
 	    S3  
 	"""
-	
-	S1=S0[1]+h/2+y 
-	S2=S0[0]+h/2+x
-	S3=S0[1]-h/2-y
-	S4=S0[0]-h/2-x
+	S1=[S0[0], S0[1]+h/2+y ]
+	S2=[S0[0]+h/2+x,S0[1]]
+	S3=[S0[0],S0[1]-h/2-y]
+	S4=[S0[0]-h/2-x,S0[1]]
+
+	# S1=S0[1]+h/2+y 
+	# S2=S0[0]+h/2+x
+	# S3=S0[1]-h/2-y
+	# S4=S0[0]-h/2-x
+
+
 
 
 	return S0,S1,S2,S3,S4
@@ -51,9 +58,13 @@ def calcShadow(P0,sunAz,sunAlt,panelAz,panelAlt,h,d):
 
 if __name__ == "__main__":
 
+	#Set Panel Properties
+	h=math.sqrt(2*(400/2)**2) #distance from centre of panel to corner [mm]
+	d=300 #Distance from the Glazed Surface
 
+	#Calculate ASF Geometry
 	#The adaptive solar facade is represented by an array of coordinates and panel size
-	panelSpacing=700 # in x y dirction[mm]
+	panelSpacing=600 # in x y dirction[mm]
 
 	xArray=4
 	yArray=3
@@ -62,39 +73,49 @@ if __name__ == "__main__":
 		ASFArray.append([])
 		for jj in range (0,xArray):
 			if ii%2==0:
-				ASFArray[ii].append([ii*panelSpacing/2,jj*panelSpacing])
+				ASFArray[ii].append([jj*panelSpacing,ii*panelSpacing/2])
 			else:
 				if jj==xArray-1:
 					pass
 				else:
-					ASFArray[ii].append([ii*panelSpacing/2,jj*panelSpacing+panelSpacing/2])
+					ASFArray[ii].append([jj*panelSpacing+panelSpacing/2,ii*panelSpacing/2])
 		
-
-	print ASFArray
-
-	P0=[0,0];
-		#Sun Position
-
-	sunAz=math.radians(40)
+	#Set Sun Position
+	sunAz=math.radians(20)
 	sunAlt=math.radians(0)
 
-	#Panel Position
-	panelAz=math.radians(-10)
-	panelAlt=math.radians(00)
+	#Set Panel Position (note that this should be an array in the future)
+	panelAz=math.radians(20)
+	panelAlt=math.radians(0)
 
-	h=math.sqrt(2*(400/2)**2) #distance from centre of panel to corner [mm]
-	d=300 #Distance from the Glazed Surface
 
+
+	#Calculate Shadow Pattern
 	shadowData=[]
 	for ii,row in enumerate(ASFArray):
 		shadowData.append([])
 		for jj,P in enumerate(row):
 			S0,S1,S2,S3,S4=calcShadow(P,sunAz,sunAlt,panelAz,panelAlt,h,d)
-			shadowData[ii].append([S0,S1,S2,S3,S4])
+			shadowData[ii].append([S1,S2,S3,S4])
 			
-	print shadowData
 
-	#S0,S1,S2,S3,S4=calcShadow(P0,sunAz,sunAlt,panelAz,panelAlt,h,d)
+
+	#Plot Data
+	for ii,row in enumerate(shadowData):
+		print 'new row'
+		for jj, Shadow in enumerate(row):
+			print Shadow
+			x,y = zip(*Shadow)
+			plt.scatter(x,y, c=np.random.rand(3,1))
+			plt.axis('equal')
+
+	
+	plt.show()
+
+
+
+	#S0,S1,S2,S3,S4=calcShadow([0,0],sunAz,sunAlt,panelAz,panelAlt,h,d)
+
 
 
 
