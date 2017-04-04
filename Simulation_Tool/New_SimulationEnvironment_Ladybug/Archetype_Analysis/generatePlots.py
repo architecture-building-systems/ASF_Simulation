fix minor bug in radiation calculation and plot results

Simulation_Tool/New_SimulationEnvironment_Ladybug/Archetype_Analysis/generatePlots.py
@@ -20,6 +20,7 @@ def archetypePlots():
 
 	ASFsimDataPath=os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..','CEA_Archetypes_CH','Archetypes_ZH_COP1_3.csv'))
 	StaticsimDataPath=os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..','CEA_Archetypes_CH','Archetypes_ZH_COP1_3_static.csv'))
+	NoASFsimDataPath=os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..','CEA_Archetypes_CH','Archetypes_ZH_COP1_3_NoASF.csv'))
 	print ASFsimDataPath
 	ASFsimData=pd.read_csv(ASFsimDataPath)
 	ASFsimData.set_index(['Name'], inplace=True)
@@ -27,6 +28,8 @@ def archetypePlots():
 	StaticsimData=pd.read_csv(StaticsimDataPath)
 	StaticsimData.set_index(['Name'], inplace=True)
 
+	NoASFsimData=pd.read_csv(NoASFsimDataPath)
+	NoASFsimData.set_index(['Name'], inplace=True)
 
 	#Define Archetypes for plotting axis labels
 	archetypes = ["MULTI_RES", "SINGLE_RES","HOTEL","OFFICE","RETAIL","FOODSTORE","RESTAURANT","INDUSTRIAL","SCHOOL","HOSPITAL","GYM"]
@@ -40,6 +43,7 @@ def archetypePlots():
 	#prepare empty numpy array for heatmap
 	ASFResults=np.zeros([6,11])
 	StaticResults=np.zeros([6,11])
+	NoASFResults=np.zeros([6,11])
 
 
 	#loop through all archetypes and place into heatmap in the correct location
@@ -49,11 +53,14 @@ def archetypePlots():
 			selectType="{0}{1}".format(archetype,str(jj))
 			ASFResults[7-jj-1,ii]=ASFsimData.loc[selectType,["E"]]
 			StaticResults[7-jj-1,ii]=StaticsimData.loc[selectType,["E"]]
+			NoASFResults[7-jj-1,ii]=NoASFsimData.loc[selectType,["E"]]
 
 
-	energySaving=StaticResults-ASFResults
+	energySaving=StaticResults - ASFResults
+	NoASFenergySaving=NoASFResults - ASFResults
+
 	#Convert to dataframe for seaborn heatmap input
-	plottingDF = pd.DataFrame(energySaving, index=yearsConstructed, columns=archetypes)
+	plottingDF = pd.DataFrame(NoASFenergySaving, index=yearsConstructed, columns=archetypes)
 	plottingDF.index.name='Year Constructed'
 
 	print plottingDF
@@ -62,7 +69,7 @@ def archetypePlots():
 	sns.set(font_scale=1.7)
 	sns.heatmap(plottingDF, linewidths=.5, cbar_kws={'label': 'Energy Saving Potential [kWh/year]'}, ax=ax)
 
-	plt.savefig('energySaving_COP1_3_static.pdf', bbox_inches='tight')
+	plt.savefig('NoASFenergySaving_COP1_3_static.pdf', bbox_inches='tight')
 	plt.show()
 
 

Simulation_Tool/New_SimulationEnvironment_Ladybug/Sensitivity/SensitivityEnvelopAnnual.py
@@ -63,10 +63,41 @@ class TestMainSimulation(unittest.TestCase):
         #     "panelSpacing":500, 
         #     "panelGridSize" : 25}
 
+        ##-----Static Simulatioh----
+        # SimulationData = {
+        #     'optimizationTypes' : ['E_total'], #, 'Cooling', 'Heating', 'SolarEnergy', 'Lighting', 'E_HCL'
+        #     'DataFolderName' : 'ZH13_49comb_static_45_0', #'ZH13_49comb_static_45_0',
+        #     'FileName': 'ZH13_49comb_static_45_0',
+        #     'geoLocation' : 'Zuerich_Kloten_2013',
+        #     'EPWfile': 'Zuerich_Kloten_2013.epw',
+        #     'Save' : False,
+        #     'ShowFig': False}
+
+        # # Set Building Parameters in [mm]
+        # BuildingData = {
+        #     "room_width": 4900,
+        #     "room_height": 3100,
+        #     "room_depth": 7000,
+        #     "glazing_percentage_w": 0.92,
+        #     "glazing_percentage_h": 0.97}
+
+        # PanelData = {
+        #     "XANGLES": [45],
+        #     "YANGLES" : [0],
+        #     "NoClusters":1,
+        #     "numberHorizontal":6,
+        #     "numberVertical":9,
+        #     "panelOffset":400,
+        #     "panelSize":400,
+        #     "panelSpacing":500, 
+        #     "panelGridSize" : 25}
+
+        ###----No ASF Simulatin -----
+
         SimulationData = {
             'optimizationTypes' : ['E_total'], #, 'Cooling', 'Heating', 'SolarEnergy', 'Lighting', 'E_HCL'
-            'DataFolderName' : 'ZH13_49comb_static_45_0', #'ZH13_49comb_static_45_0',
-            'FileName': 'ZH13_49comb_static_45_0',
+            'DataFolderName' : 'ZH13_NoASF', #'ZH13_49comb_static_45_0',
+            'FileName': 'ZH13_NoASF',
             'geoLocation' : 'Zuerich_Kloten_2013',
             'EPWfile': 'Zuerich_Kloten_2013.epw',
             'Save' : False,
@@ -81,11 +112,11 @@ class TestMainSimulation(unittest.TestCase):
             "glazing_percentage_h": 0.97}
 
         PanelData = {
-            "XANGLES": [45],
+            "XANGLES": [0],
             "YANGLES" : [0],
             "NoClusters":1,
-            "numberHorizontal":6,
-            "numberVertical":9,
+            "numberHorizontal":0,
+            "numberVertical":0,
             "panelOffset":400,
             "panelSize":400,
             "panelSpacing":500, 
@@ -103,14 +134,15 @@ class TestMainSimulation(unittest.TestCase):
             "Temp_start" : 20, 
             'human_heat_emission' : 0.12,}
 
-        U_wRange=np.arange(0.2,4.1,0.2)
-        print U_wRange
+        #U_Range=np.arange(0.2,4.1,0.2)
+        U_Range=np.arange(0.2,2.1,0.2)
 
-        all_results=pd.DataFrame({'U_envelope': []})
-        all_results.set_index(['U_envelope'], inplace=True)
+
+        all_results=pd.DataFrame({'Infiltration': []})
+        all_results.set_index(['Infiltration'], inplace=True)
 
         # loop through building properties and simulation options dictionaries:
-        for ii,sens in enumerate(U_wRange): #range(0, len(runlist)):
+        for ii,sens in enumerate(U_Range): #range(0, len(runlist)):
 
 
             BuildingProperties={
@@ -121,9 +153,9 @@ class TestMainSimulation(unittest.TestCase):
                 "Lighting_Utilisation_Factor" :  0.45,
                 "Lighting_Maintenance_Factor" : 0.9,
                 "U_em" : 0.2, 
-                "U_w" : sens,
+                "U_w" : 1.2,
                 "ACH_vent" : 1.5,
-                "ACH_infl" :0.5,
+                "ACH_infl" :sens,
                 "ventilation_efficiency" : 0.6 ,
                 "c_m_A_f" : 165 * 10**3,
                 "theta_int_h_set" : 20,
@@ -142,8 +174,8 @@ class TestMainSimulation(unittest.TestCase):
 
             # Add building U_envelope to dataframe and append subsequent iterations:
             current_result = ASF_archetype.yearlyData.T
-            current_result['U_envelope'] = sens
-            current_result.set_index(['U_envelope'], inplace=True)
+            current_result['Infiltration'] = sens
+            current_result.set_index(['Infiltration'], inplace=True)
             temp_list = [all_results, current_result] #TODO: Change this to one line
             all_results = pd.concat(temp_list) 
 

Simulation_Tool/New_SimulationEnvironment_Ladybug/SimulationClass.py
@@ -227,21 +227,22 @@ class ASF_Simulation(object):
 
 		
 	def runRadiationCalculation(self):
-		 
-         from RadiationCalculation import CalculateRadiationData
-         from asf_electricity_production_2 import asf_electricity_production
 
-	   #Calculate the Radiation on the solar panels and window with ladybug
-         self.BuildingRadiationData_HOD = CalculateRadiationData(XANGLES = self.XANGLES, 
+		from RadiationCalculation import CalculateRadiationData
+		from asf_electricity_production_2 import asf_electricity_production
+
+	   	#Calculate the Radiation on the solar panels and window with ladybug
+		self.BuildingRadiationData_HOD = CalculateRadiationData(XANGLES = self.XANGLES, 
                                                                 YANGLES = self.YANGLES, 
                                                                 paths = self.paths,
                                                                 daysPerMonth = self.daysPerMonth, 
                                                                 hour_in_month = self.hour_in_month,
                                                                 FolderName = self.SimulationData)
 			
-		
-	   #if there are no panels vertical and horizontal
-         if self.PanelData['numberHorizontal'] == 0 and self.PanelData['numberVertical'] == 0:
+		print self.PanelData['numberHorizontal']
+		#if there are no panels vertical and horizontal
+		if self.PanelData['numberHorizontal'] == 0 and self.PanelData['numberVertical'] == 0:
+			print 'hellow'
 			self.PV_electricity_results = {}
 			
 			self.PV_electricity_results['Pmpp_sum'] = np.array(self.NumberCombinations * len(self.hour_in_month) * [0])
@@ -249,7 +250,7 @@ class ASF_Simulation(object):
 			
 		
 	   #with the radiation_results the Pv_results are calcualted, make sure you know where the results are saved, otherwise they will just be loaded
-         else:
+		else:
         		if not os.path.isfile(os.path.join(self.paths['PV'], 'PV_electricity_results_' + self.SimulationData['FileName'] + '.npy')): 
         			if not os.path.isdir(self.paths['PV']):
         				os.makedirs(self.paths['PV'])
@@ -550,12 +551,18 @@ class ASF_Simulation(object):
 		
 
             self.initializeASF()
-            self.setBuildingParameters()		
+            print 'setting parameters'
+            self.setBuildingParameters()
+            print 'initialising'		
             self.initializeBuildingSimulation()
             self.setPaths()				
+            print 'calc variavbles'
             self.CalculateVariables()
-            self.runRadiationCalculation()		  		 
-            self.PrepareRadiationData()													   
+            print 'run radiation calc'
+            self.runRadiationCalculation()	
+            print 'prepare rad data'	  		 
+            self.PrepareRadiationData()	
+            print 'running simulation'												   
             self.runBuildingSimulation()
             
             if self.SimulationData['ShowFig'] or self.SimulationData['Save']:   